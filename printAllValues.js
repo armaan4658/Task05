968b0fc53254529a6258017b2a0240501ca51dae
armaan4658 <80997749+armaan4658@users.noreply.github.com>
2021-04-20 16:01:41 +0530

    Update printAllValues.js

--- printAllValues.js
+++ printAllValues.js
@@ -1,10 +1,5 @@
 var obj = {name : 'RajiniKanth', age : 33, hasPets : false};
-
 function printAllValues(obj) {
-   let list = [];
-   for (let i in obj){
-       list.push(obj[i]);
-   }
-   console.log(list);
+   console.log(Object.values(obj));
 }
 printAllValues(obj);
