c9a26ffba83dc6c7d6b6105873931df703d4682f
armaan4658 <80997749+armaan4658@users.noreply.github.com>
2021-04-21 08:56:38 +0530

    Update fromListToObject.js

--- fromListToObject.js
+++ fromListToObject.js
@@ -1,9 +1,9 @@
 var arr = [['“make”', '“Ford”'], ['“model”', '“Mustang”'], ['“year”', 1964]];
 function fromListToObject(arr) {
- var newObject = {};
+ var newObject = {};//creating new object
  for(let i in arr){
-     newObject[arr[i][0]]=arr[i][1];
+     newObject[arr[i][0]]=arr[i][1];//adding key:value to the object
  }
  return newObject;
 }
-console.log(fromListToObject(arr));
+console.log(fromListToObject(arr));//printing out the output
