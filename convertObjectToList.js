e17e57261421af89e254e65f1a025cc944a6453b
armaan4658 <80997749+armaan4658@users.noreply.github.com>
2021-04-21 08:55:26 +0530

    Update convertObjectToList.js

--- convertObjectToList.js
+++ convertObjectToList.js
@@ -1,16 +1,16 @@
 var obj = {name: '“ISRO”', age: 35, role: '“Scientist”'};
 function convertListToObject(obj) {
  // your code here
- let keys = Object.keys(obj);
- let array = [];
- for(let i in obj){
+ let keys = Object.keys(obj);//getting all the keys of "obj"
+ let array = [];//creating an empty array
+ for(let i in obj){//using for in loop to iterate through the obj
      let index=0;
      let list = [];
-     list.push(keys[index]);
-     list.push(obj[i]);
+     list.push(keys[index]);//pushing key
+     list.push(obj[i]);//pushing value
      array.push(list);
      index++;
  }
- console.log(array);
+ console.log(array);//printing out the output
 }
-convertListToObject(obj);
+convertListToObject(obj);//calling function
